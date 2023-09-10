2698d43af9f740dfa4f290e44441236fb47777bb
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2023-09-10 15:00:45 +0700

    styling(sidebar): add content & responsiveness to sidebar

--- src/components/cardplace.js
+++ src/components/cardplace.js
@@ -4,16 +4,19 @@ import Thumb from '../assets/thumb.jpeg'
 // Style
 import style from '../style/CardPlace.module.css'
 export default function CardPlace({ data, getSelected }) {
-    const { name } = data
+    const { name, hour } = data
 
     return (
-        <div className={style.card}>
-            <img src={Thumb} className={style.thumb_card} alt="place pict" />
-            <div className={style.content_card}>
-                <h1 className={style.content_title}>{name}</h1>
-                <span>Alamat</span>
-                <button onClick={() => getSelected(data)} className={style.button_card}>Details</button>
+        <>
+            <div className={style.card}>
+                <img src={Thumb} className={style.thumb_card} alt="place pict" />
+                <div className={style.content_card}>
+                    <h1 className={style.content_title}>{name}</h1>
+                    <span className={style.content_hour}>{hour}</span>
+                    <button onClick={() => getSelected(data)} className={style.button_card}>Details</button>
+                </div>
             </div>
-        </div>
+            <hr className={style.border_card} />
+        </>
     )
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,50 +1,154 @@
-// Library
-import { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-// Components
-import CardPlace from './cardplace'
+import { useMediaQuery } from 'react-responsive';
+import CardPlace from './cardplace';
 import { setCenter, setZoom } from '../store/setupSlice';
 
-// Asset
-import Thumb from '../assets/thumb.jpeg'
-import Arrow from '../assets/arrow-left-white.svg'
+import Thumb from '../assets/thumb.jpeg';
+import Arrow from '../assets/arrow-left-white.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import MapIcon from '@mui/icons-material/Map';
 
-
-// Style
-import style from '../style/Sidebar.module.css'
+import style from '../style/Sidebar.module.css';
 
 export default function SideBar() {
     const { zoom_default, middle, marks } = useSelector((state) => state.setup.setup);
-
-    const [selected, setSelected] = useState(null)
     const dispatch = useDispatch();
 
+    const [selected, setSelected] = useState(null);
+    const [expand, setExpand] = useState(false);
+    const [hide, setHide] = useState(false);
+    const sidebarRef = useRef(null); 
+
+    // Responsive
+    const isResponsive = useMediaQuery({ maxWidth: 992 });
+
     function getSelected(value) {
-        setSelected(value)
+        setSelected(value);
         dispatch(setCenter([value.lot, value.lat]));
-        dispatch(setZoom(18))
+        dispatch(setZoom(18));
     }
 
     function removeSelected() {
-        setSelected(null)
+        setSelected(null);
         dispatch(setCenter(middle.cordinate));
-        dispatch(setZoom(zoom_default))
+        dispatch(setZoom(zoom_default));
     }
 
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+                setExpand(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <aside className={style.sidebar}>
+        <aside className={style.sidebar} ref={sidebarRef}>
             {selected ? (
                 <section className={style.place_list_container}>
-                    <button className={style.back_button} onClick={() => removeSelected()}><img src={Arrow} alt="arrow icon" /></button>
-                    <img src={Thumb} alt="thumb-detail" />
-                    <h1>{selected.name}</h1>
+                    {isResponsive ? (
+                        <div className={`${style.content_information} ${hide ? style.hide : ''}`}>
+                            <div style={{position: 'relative'}}>
+                            <button className={style.back_button} style={{top: '15px'}} onClick={() => {removeSelected(); setExpand(true)}}>
+                                <img src={Arrow} alt="arrow icon" />
+                            </button>
+                                <button className={`${style.content_back_button} ${hide ? style.hide : ''}`} onClick={() => {setHide(!hide); setExpand(!expand)}}>
+                                    <img src={Arrow} alt="arrow icon" />
+                                </button>
+                            </div>
+                            <img src={Thumb} alt="thumb-detail" />
+
+                            <div className={style.place_information}>
+                                <div className={style.base_info}>
+                                    <p>{selected.name}</p>
+                                    <div style={{display: 'flex',flexDirection: 'column', gap: '5px'}}>
+                                        <span>Minimarket</span>
+                                        <span>{selected.hour}</span>
+                                    </div>
+                                </div>
+                                <ul className={style.place_more_info}>
+                                    <li>
+                                        <LocationOnIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <span>{selected.address}</span>
+                                    </li>
+                                    <li>
+                                        <MapIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <span>{`Lot: ${selected.lot}, Lat: ${selected.lat}`}</span>
+                                    </li>
+                                    <li>
+                                        <DirectionsIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <a rel='noreferrer' target="_blank" href={selected.rute}>Lihat rute disini</a>
+                                    </li>
+                                </ul>
+                                <div className={style.place_photo_section}>
+                                <p>Foto</p>
+                                {/* Map Photo */}
+                                </div>
+                            </div>
+                        </div>
+                    ): (
+                        <>
+                            <button className={style.back_button} onClick={() => {removeSelected(); setExpand(true)}}>
+                                <img src={Arrow} alt="arrow icon" />
+                            </button>
+                            <img src={Thumb} alt="thumb-detail" />
+                            <div className={style.place_information}>
+                                <div className={style.base_info}>
+                                    <p>{selected.name}</p>
+                                    <div style={{display: 'flex',flexDirection: 'column', gap: '5px'}}>
+                                        <span>Minimarket</span>
+                                        <span>{selected.hour}</span>
+                                    </div>
+                                </div>
+                                <ul className={style.place_more_info}>
+                                    <li>
+                                        <LocationOnIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <span>{selected.address}</span>
+                                    </li>
+                                    <li>
+                                        <MapIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <span>{`Lot: ${selected.lot}, Lat: ${selected.lat}`}</span>
+                                    </li>
+                                    <li>
+                                        <DirectionsIcon sx={{ color: '#FF9505', width: '32px'}} />
+                                        <a href={selected.rute}>Lihat rute disini</a>
+                                    </li>
+                                </ul>
+                                <div className={style.place_photo_section}>
+                                <p>Foto</p>
+                                {/* Map Photo */}
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </section>
             ) : (
-                <section className={style.place_list_container}>
-                    {marks.map((mark, index) => <CardPlace data={mark} key={index} getSelected={getSelected} />)}
+                <section className={`${style.place_list_container}`}>
+                    {isResponsive ? (
+                        <>
+                            <div className={style.menu_icon}>
+                                <MenuIcon className={style.icon_sidebar} onClick={() => setExpand(!expand)} />
+                            </div>
+
+                            <div className={`${style.content} ${expand ? style.show : ''}`}>
+                                {marks.map((mark, index) => (
+                                    <CardPlace data={mark} key={index} getSelected={getSelected} />
+                                ))}
+                            </div>
+                        </>
+                    ) : (
+                        marks.map((mark, index) => <CardPlace data={mark} key={index} getSelected={getSelected} />)
+                    )}
                 </section>
             )}
         </aside>
-    )
+    );
 }
