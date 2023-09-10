5938aec1ffe4aab59b8fc5a751cfb7e8ff176182
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-09-11 02:08:00 +0700

    style(card-place): change thumb card become background image and adjust responsiveness layout

--- src/components/cardplace.js
+++ src/components/cardplace.js
@@ -7,13 +7,13 @@ export default function CardPlace({ data, getSelected }) {
     return (
         <>
             <div className={style.card}>
-                    <img src={image} className={style.thumb_card} alt="place pict" />
+                <div className={style.thumb_card} style={{ backgroundImage: `url(${image})` }}></div>
                 <div className={style.content_card}>
                     <h1 className={style.content_title}>{name}</h1>
                     <span className={style.content_hour}>{hour}</span>
                     <button onClick={() => getSelected(data)} className={style.button_card}>Details</button>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
